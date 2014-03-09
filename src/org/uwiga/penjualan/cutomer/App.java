177caf318c3ebf86730d4f7c66cf127aa9d71127
hadi <hadi@hadi0110>
2014-03-09 12:08:29 +0700

    tambahan hadi2

--- src/org/uwiga/penjualan/cutomer/App.java
+++ src/org/uwiga/penjualan/cutomer/App.java
@@ -2,14 +2,20 @@ package org.uwiga.penjualan.cutomer;
 
 public class App {
 	public App(){
-		
+		System.out.println("Coba Tambahan saya");
+	}
+	public void tambahanHadi(){
+		System.out.println("tambahan");
 	}
 
 	public static void main(String[] args) {
+		App app = new App();
 		MasterCustomerModel Model = new MasterCustomerModel();
 		MasterCustomerView View = new MasterCustomerView(Model);
 		MasterCustomerController controller = new MasterCustomerController(Model,View);
 		View.setVisible(true);
+		app.tambahanHadi();
+		System.out.println("Finish Loading");
 	}
 
 }
